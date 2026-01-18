b2fdbe52c9bad5376ed7468cce3697368ec6c0bd
Harikrishna9875 <harikrishnaravanaveni@gmail.com>
2026-01-18 22:03:50 +0530

    Fix logout to POST only

--- config/urls.py
+++ config/urls.py
@@ -5,13 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
     # Your app
     path("", include("core.urls")),
+    path("admin/", admin.site.urls),
 
-    # Django auth (login, logout, password reset, etc.)
-    path("accounts/", include("django.contrib.auth.urls")),
+    
+
+   
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
